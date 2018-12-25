cd691e5ccc2d12500d2458b0fe908c8c88cbd8cc
Sakthivel Balasubramaniam <sakthivel.b@thoughtworks.com>
2018-12-25 12:54:09 +0530

    Array was replaced by Circular linked list to store marble list, this optimized the program to far better levels

--- src/advent/day_nine.go
+++ src/advent/day_nine.go
@@ -5,17 +5,23 @@ import (
 	"regexp"
 )
 
+type marble struct {
+	value int
+	next  *marble
+	prev  *marble
+}
+
 // MarbleMania is Elf play area
 func MarbleMania() {
 	fmt.Println("--- Marble Mania ---")
 
 	input := ""
 	// input = "10 players; last marble is worth 1618 points"
-	input = "13 players; last marble is worth 7999 points"
+	// input = "13 players; last marble is worth 7999 points"
 	// input = "17 players; last marble is worth 1104 points"
 	// input = "21 players; last marble is worth 6111 points"
-	input = "30 players; last marble is worth 5807 points"
-	input = "430 players; last marble is worth 71588 points"
+	// input = "30 players; last marble is worth 5807 points"
+	input = "430 players; last marble is worth 7158800 points" //3412522480
 	// input = "9 players; last marble is worth 25 points"
 
 	var pattern = "(?P<players>[0-9]*) players; last marble is worth (?P<marbles>[0-9]*) points"
@@ -25,6 +31,77 @@ func MarbleMania() {
 	nosMarbles := ConvInt(matches[2])
 	// fmt.Println("# of players =", nosPlayers, ", # of marbles =", nosMarbles)
 
+	// playerScoreMap := getElfScores(nosPlayers, nosMarbles)
+	playerScoreMap := getElfScoresCLL(nosPlayers, nosMarbles)
+
+	//fmt.Println(playerScoreMap)
+
+	fmt.Println("max score =", getMaxScore(playerScoreMap))
+}
+
+func getElfScoresCLL(nosPlayers, nosMarbles int) map[int]int {
+	fmt.Println("# of players =", nosPlayers, ", # of marbles =", nosMarbles)
+
+	playerScoreMap := make(map[int]int)
+	player := 0
+
+	header := &marble{value: 0}
+	nextElem := &marble{value: 1}
+
+	header.next = nextElem
+	header.prev = nextElem
+
+	nextElem.next = header
+	nextElem.prev = header
+
+	// updating header
+
+	for currentMarble := 2; currentMarble <= nosMarbles; currentMarble++ {
+		player = (player + 1) % nosPlayers
+
+		if currentMarble%23 == 0 {
+			playerScoreMap[player] += currentMarble
+			for i := 0; i < 7; i++ {
+				header = header.prev
+			}
+			playerScoreMap[player] += header.value
+
+			removeNode := header
+			removeNode.next.prev = removeNode.prev
+			removeNode.prev.next = removeNode.next
+
+			header = removeNode.next
+		} else {
+			newNode := &marble{value: currentMarble,
+				next: header.next.next, // to jump two nodes
+				prev: header.next}
+			newNode.prev.next = newNode
+			newNode.next.prev = newNode
+			header = newNode
+		}
+	}
+
+	return playerScoreMap
+}
+
+func print(header *marble) {
+	temp := header.next
+	for i := 0; temp != header; i++ {
+		fmt.Print(temp.value, ", ")
+		temp = temp.next
+	}
+	fmt.Print(temp.value, "\n")
+}
+func getMaxScore(playerScoreMap map[int]int) int {
+	max := 0
+	for _, score := range playerScoreMap {
+		if max < score {
+			max = score
+		}
+	}
+	return max
+}
+func getElfScores(nosPlayers, nosMarbles int) map[int]int {
 	playerScoreMap := make(map[int]int)
 	marbleList := []int{0}
 
@@ -37,13 +114,15 @@ func MarbleMania() {
 		if i%23 == 0 {
 			playerScoreMap[player] += i
 
-			placeIndex = (placeIndex - 7 - 2) % len(marbleList)
-			if placeIndex < 0 {
-				placeIndex = -placeIndex
+			// currentIndex := (placeIndex - 7 - 2) % len(marbleList)
+			currentIndex := (placeIndex - 7 - 2)
+			if currentIndex < 0 {
+				currentIndex = len(marbleList) + currentIndex
 			}
 			// fmt.Println("placeIndex =", placeIndex)
-			playerScoreMap[player] += marbleList[placeIndex]
-			marbleList = RemoveAtIndex(marbleList, placeIndex)
+			playerScoreMap[player] += marbleList[currentIndex]
+			marbleList = RemoveAtIndex(marbleList, currentIndex)
+			placeIndex = currentIndex
 		} else {
 			marbleList = AppendAtIndex(marbleList, placeIndex, i)
 		}
@@ -59,13 +138,5 @@ func MarbleMania() {
 
 		// fmt.Println("Player #", player, "-", marbleList)
 	}
-
-	// fmt.Println(playerScoreMap)
-	max := 0
-	for _, score := range playerScoreMap {
-		if max < score {
-			max = score
-		}
-	}
-	fmt.Println("max score =", max)
+	return playerScoreMap
 }
